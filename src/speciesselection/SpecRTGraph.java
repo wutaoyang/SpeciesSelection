b779ef6f284373e7aa5db1e94a5b149a92c6e4b6
mre16utu <mre16utu@cmp-18internpc.UEA.AC.UK>
2018-01-31 13:30:24 +0000

    Code tidy up Added Visual Paradigm project

--- src/speciesselection/SpecRTGraph.java
+++ src/speciesselection/SpecRTGraph.java
@@ -14,353 +14,298 @@ import java.util.*;
  * @author taoyang wu @UEA (taoyang.wu@uea.ac.uk)
  */
 public class SpecRTGraph {
-    
+
     //The bipartite graph is represented by two 'adjacent' lists
     //Here each species is associated with a list of resorce types (RT), and
     //each RT is associated with a list of species.
     protected ArrayList<Species> specNode;
     protected ArrayList<ResourceType> rtNode;
-    
-    SpecRTGraph(){
-        specNode=new ArrayList<Species>();
-        rtNode=new ArrayList<ResourceType>();
+
+    SpecRTGraph() {
+        specNode = new ArrayList<>();
+        rtNode   = new ArrayList<>();
     }
-    
-    public void addSpecNode(Species inSpecies){
-        if(specNode.contains(inSpecies)==false)
+
+    public void addSpecNode(Species inSpecies) {
+        if (specNode.contains(inSpecies) == false) {
             specNode.add(inSpecies);
+        }
     }
-    
-    public int numSpecies(){
+
+    public int numSpecies() {
         return specNode.size();
     }
-    
-    public int numResourceTypes(){
+
+    public int numResourceTypes() {
         return rtNode.size();
     }
-    
-    public void addRTNode(ResourceType newRT){
-        if(rtNode.contains(newRT)==false)
-        rtNode.add(newRT);
+
+    public void addRTNode(ResourceType newRT) {
+        if (rtNode.contains(newRT) == false) {
+            rtNode.add(newRT);
+        }
     }
-    
-    public boolean containsSpecies(Species spec){
+
+    public boolean containsSpecies(Species spec) {
         return specNode.contains(spec);
     }
-    
-    public boolean containsResourceType(ResourceType rt){
+
+    public boolean containsResourceType(ResourceType rt) {
         return rtNode.contains(rt);
     }
-    
-    
-      
-    public boolean noIsolatedResourceType(){
-        for (ResourceType rt:rtNode){
-            if(rt.numSpecies()==0){
+
+    public boolean noIsolatedResourceType() {
+        for (ResourceType rt : rtNode) {
+            if (rt.numSpecies() == 0) {
                 System.out.println("There is an isolated resource type.");
                 return false;
             }
         }
         return true;
     }
-    
-    
-    public ResourceType getResourceTypeByID(int rtValue){
-         
-        for(ResourceType tmpRT:rtNode){
-            if(tmpRT.getID()==rtValue)
+
+    public ResourceType getResourceTypeByID(int rtValue) {
+
+        for (ResourceType tmpRT : rtNode) {
+            if (tmpRT.getID() == rtValue) {
                 return tmpRT;
+            }
         }
-        
-        return null;        
+
+        return null;
     }
-    
-    
-    public void addEdge(Species specEnd, ResourceType rtEnd){
+
+    public void addEdge(Species specEnd, ResourceType rtEnd) {
         addSpecNode(specEnd);
         addRTNode(rtEnd);
-        
+
         specEnd.addResourceType(rtEnd);
-        rtEnd.addSpecies(specEnd);        
-    } 
-    
-      
-    
-    public SpecSetFamily getDomSpecSets(int setSize, int targetNum, MinSpecSetFamily mssf){
+        rtEnd.addSpecies(specEnd);
+    }
+
+    public SpecSetFamily getDomSpecSets(int setSize, int targetNum, MinSpecSetFamily mssf) {
         mssf.sortBySensitivity();
         Collections.sort(rtNode);
-        
-       
-        
-        SpecSetFamily result=new SpecSetFamily();
+
+        SpecSetFamily result = new SpecSetFamily();
         result.setNumSetsLimit(targetNum);
-        
-        int maxRTex=mssf.size();
-        for(int i=0;i<maxRTex;i++){
-            SpecSet tmpSet=mssf.get(i);
-            if(tmpSet.size()<=setSize){
-                SpecSetFamily tmpFamily=tmpSet.extendToFamily(specNode, setSize, targetNum);
-                result.addSpecSet(tmpFamily);               
+
+        int maxRTex = mssf.size();
+        for (int i = 0; i < maxRTex; i++) {
+            SpecSet tmpSet = mssf.get(i);
+            if (tmpSet.size() <= setSize) {
+                SpecSetFamily tmpFamily = tmpSet.extendToFamily(specNode, setSize, targetNum);
+                result.addSpecSet(tmpFamily);
             }
-            
         }
-        
-    
         return result;
-    
     }
-    
-    
-   /**
-    * 
-    * @param setSize   The number of species in the species set
-    * @param targetNum The number of species sets to be computed
-    * @return A collection of species sets
-    */
-    public SpecSetFamily getDomSpecSets(int setSize,int targetNum){
-        MinSpecSetFamily mssf=this.getMinDomSpecSets();
-        return getDomSpecSets(setSize, targetNum,mssf);
-       
-    }
-    
-    
+
     /**
-     * 
+     *
+     * @param setSize The number of species in the species set
+     * @param targetNum The number of species sets to be computed
+     * @return A collection of species sets
+     */
+    public SpecSetFamily getDomSpecSets(int setSize, int targetNum) {
+        MinSpecSetFamily mssf = this.getMinDomSpecSets();
+        return getDomSpecSets(setSize, targetNum, mssf);
+    }
+
+    /**
+     *
      * @return The collection of 'irreducible' dominated sets
      */
-    
-    
-    public MinSpecSetFamily getMinDomSpecSets(){
-        MinSpecSetFamily result=new MinSpecSetFamily();
-        
-        
-       ResourceType minRT=this.getFirstEssentialResourceType();
-       
-       if(minRT==null || minRT.noSpecies() ){
-           System.out.println("There is something wrong with the "
-                   + "minimal rticator, such as adjacent to no species. ");
-       }
-       
-       
-       for(Species spec:minRT.specList){        
-       
-               
-        SpecTree minTree=this.getMinimalConstSpecTreeRootedAt(spec);
-        
-          
-        ArrayList<SpecTreeNode> leafList=minTree.getLeaves();
-        
-        for(SpecTreeNode leaf:leafList){
-            ArrayList<Species> sp=new ArrayList<Species>();
-            sp=leaf.getAncestors();
-            SpecSet tmpSet=new SpecSet(sp);
-            result.addSpecSet(tmpSet);            
-        }        
-       }
+    public MinSpecSetFamily getMinDomSpecSets() {
+        MinSpecSetFamily result = new MinSpecSetFamily();
+        ResourceType minRT = this.getFirstEssentialResourceType();
 
+        if (minRT == null || minRT.noSpecies()) {
+            System.out.println("There is something wrong with the "
+                    + "minimal rticator, such as adjacent to no species. ");
+        }
+
+        for (Species spec : minRT.specList) {
+            SpecTree minTree = this.getMinimalConstSpecTreeRootedAt(spec);
+            ArrayList<SpecTreeNode> leafList = minTree.getLeaves();
+
+            for (SpecTreeNode leaf : leafList) {
+                ArrayList<Species> sp = leaf.getAncestors();
+                SpecSet tmpSet = new SpecSet(sp);
+                result.addSpecSet(tmpSet);
+            }
+        }
         return result;
     }
-    
-    
-    public SpecTree getMinSpecTree(){
-     ArrayList<ResourceType> essRT=this.getEssentialResourceTypes();
-     Species root=essRT.get(0).getFirstSpecies();
-     
-     return this.getMinimalConstSpecTreeRootedAt(root);
-        
-        
+
+    public SpecTree getMinSpecTree() {
+        ArrayList<ResourceType> essRT = this.getEssentialResourceTypes();
+        Species root = essRT.get(0).getFirstSpecies();
+
+        return this.getMinimalConstSpecTreeRootedAt(root);
+
     }
-    
-    
+
     private boolean isDominated(
-            ArrayList<Species> specList, ArrayList<ResourceType> rtList){
-        return (getFirstNonDominatedRT(specList,rtList)==null)?true:false;
+            ArrayList<Species> specList, ArrayList<ResourceType> rtList) {
+        return (getFirstNonDominatedRT(specList, rtList) == null) ? true : false;
     }
-    
-    
-    
+
     /**
-     * 
+     *
      * @param rootSpec A species
-     * @return A minimal constitute species tree with root 
-     * specified by rootSpec; null if rootSpec is not a  species
+     * @return A minimal constitute species tree with root specified by
+     * rootSpec; null if rootSpec is not a species
      */
-    
-   
-    
-    public SpecTree getMinimalConstSpecTreeRootedAt(Species rootSpec){
-        if(!this.containsSpecies(rootSpec)) return null;
-        
-        ArrayList<ResourceType> essRT=this.getEssentialResourceTypes();
-        
-        SpecTreeNode rootNode=new SpecTreeNode(rootSpec);
-        
-        SpecTree returnTree=new SpecTree(rootNode);
-        
-            
-        
-        while(getFirstNonCompleteLeaf(returnTree,essRT)!=null){
-            
-                                
-            SpecTreeNode curNode=getFirstNonCompleteLeaf(returnTree,essRT);
-            ResourceType curRT=getFirstNonDominatedRT(curNode.getAncestors(),essRT);
-            
-                  
-            ArrayList<Species> specList=curRT.specList;
-            for(Species spec: specList){
-                SpecTreeNode tmpNode=new SpecTreeNode(spec);
+    public SpecTree getMinimalConstSpecTreeRootedAt(Species rootSpec) {
+        if (!this.containsSpecies(rootSpec)) {
+            return null;
+        }
+
+        ArrayList<ResourceType> essRT = this.getEssentialResourceTypes();
+
+        SpecTreeNode rootNode = new SpecTreeNode(rootSpec);
+
+        SpecTree returnTree = new SpecTree(rootNode);
+
+        while (getFirstNonCompleteLeaf(returnTree, essRT) != null) {
+
+            SpecTreeNode curNode = getFirstNonCompleteLeaf(returnTree, essRT);
+            ResourceType curRT = getFirstNonDominatedRT(curNode.getAncestors(), essRT);
+
+            ArrayList<Species> specList = curRT.specList;
+            for (Species spec : specList) {
+                SpecTreeNode tmpNode = new SpecTreeNode(spec);
                 curNode.addChild(tmpNode);
             }
         }
-        
-           
+
         return returnTree;
-        
+
     }
-    
-    
+
     private SpecTreeNode getFirstNonCompleteLeaf(
-            SpecTree st, ArrayList<ResourceType> rtList ){
-        
-          
-        ArrayList<SpecTreeNode> leafList=st.getLeaves();
-        
-        for(SpecTreeNode stn: leafList){
-            if(getFirstNonDominatedRT(stn.getAncestors(),rtList)!=null)
-            return stn;
+            SpecTree st, ArrayList<ResourceType> rtList) {
+
+        ArrayList<SpecTreeNode> leafList = st.getLeaves();
+
+        for (SpecTreeNode stn : leafList) {
+            if (getFirstNonDominatedRT(stn.getAncestors(), rtList) != null) {
+                return stn;
+            }
         }
-        
+
         return null;
-        
+
     }
-    
-    
+
     /**
-     * 
+     *
      * @param specList A collection of species node
-     * @param rtList  A collection of ResourceType node
-     * @return The first RT that is not adjacent to any 
-     *    of the species in specList, null if no such RT exists
+     * @param rtList A collection of ResourceType node
+     * @return The first RT that is not adjacent to any of the species in
+     * specList, null if no such RT exists
      */
-    
     private ResourceType getFirstNonDominatedRT(
-            ArrayList<Species> specList, ArrayList<ResourceType> rtList){
-        
-      //  System.out.println("getFirstNonDOminatedRT");
-       // System.out.println("The species set is "+specList);
-       // System.out.println("The rticator set is "+rtList);
-        
-        for(ResourceType rt: rtList){
-            boolean dominated=false;
-            for(Species spec: specList){
-                if(rt.adjacentTo(spec)){
-                   dominated=true;          
-                  
+            ArrayList<Species> specList, ArrayList<ResourceType> rtList) {
+
+        //  System.out.println("getFirstNonDOminatedRT");
+        // System.out.println("The species set is "+specList);
+        // System.out.println("The rticator set is "+rtList);
+        for (ResourceType rt : rtList) {
+            boolean dominated = false;
+            for (Species spec : specList) {
+                if (rt.adjacentTo(spec)) {
+                    dominated = true;
+
                 }
             }
-            if(dominated==false){
-            // System.out.println("The return rticator is "+rt);
+            if (dominated == false) {
+                // System.out.println("The return rticator is "+rt);
                 return rt;
             }
         }
-        return null;       
+        return null;
     }
-    
-    
-    
-    private ResourceType getFirstEssentialResourceType(){
-        ArrayList<ResourceType> rtList=this.getEssentialResourceTypes();
-        if(rtList.isEmpty()){
+
+    private ResourceType getFirstEssentialResourceType() {
+        ArrayList<ResourceType> rtList = this.getEssentialResourceTypes();
+        if (rtList.isEmpty()) {
             return null;
         }
-        
+
         return rtList.get(0);
     }
-     
-    
-    
 
-    
-    
-    
-    
     /**
-     * 
-     * @return Return a set of essential rticators, when an rticator
-     * is essential if it is not covered by other rticators
+     *
+     * @return Return a set of essential rticators, when an rticator is
+     * essential if it is not covered by other rticators
      */
-    
-    
-    public ArrayList<ResourceType> getEssentialResourceTypes(){
-        
+    public ArrayList<ResourceType> getEssentialResourceTypes() {
+
         //sorting the rticator list first
         Collections.sort(rtNode);
-        
+
         ArrayList<ResourceType> essRT;
-        essRT=new ArrayList<ResourceType>();
-        
-        int rtSize=numResourceTypes();
-        
-        for(int i=0;i<rtSize;i++){
-            ResourceType curRT=rtNode.get(i);
-            boolean essFlag=true; //false rtications no essential
-            
-            for(int j=i+1;j<rtSize;j++){
-                if(curRT.isCoveredBy(rtNode.get(j))){
-                    essFlag=false;
+        essRT = new ArrayList<ResourceType>();
+
+        int rtSize = numResourceTypes();
+
+        for (int i = 0; i < rtSize; i++) {
+            ResourceType curRT = rtNode.get(i);
+            boolean essFlag = true; //false rtications no essential
+
+            for (int j = i + 1; j < rtSize; j++) {
+                if (curRT.isCoveredBy(rtNode.get(j))) {
+                    essFlag = false;
                     break;
-                }                    
+                }
             } //end for
-            
-            for(ResourceType tmpRT: essRT){
-                if(curRT.isCoveredBy(tmpRT)){
-                    essFlag=false;
+
+            for (ResourceType tmpRT : essRT) {
+                if (curRT.isCoveredBy(tmpRT)) {
+                    essFlag = false;
                     break;
                 }
             }
-            
-            if(essFlag==true){
+
+            if (essFlag == true) {
                 essRT.add(curRT);
             }
         }
-        
-       // System.out.println(essRT);
-        
+
+        // System.out.println(essRT);
         return essRT;
     }
-    
-    public ArrayList<Species> getOneMinimalConstituteSpecies(){
-        ArrayList<Species> conSpecList=new ArrayList<Species>();
-        ArrayList<ResourceType> essRTList=new ArrayList<ResourceType>();
-    
-        essRTList=getEssentialResourceTypes();
-        
-        while(essRTList.size()!=0){
-            ResourceType tmpRT=essRTList.get(0); //get the first essRTList
-            Species tmpSpec=tmpRT.getFirstSpecies();
+
+    public ArrayList<Species> getOneMinimalConstituteSpecies() {
+        ArrayList<Species> conSpecList = new ArrayList<Species>();
+        ArrayList<ResourceType> essRTList = new ArrayList<ResourceType>();
+
+        essRTList = getEssentialResourceTypes();
+
+        while (essRTList.size() != 0) {
+            ResourceType tmpRT = essRTList.get(0); //get the first essRTList
+            Species tmpSpec = tmpRT.getFirstSpecies();
             conSpecList.add(tmpSpec);
-            removeDomination(essRTList,tmpSpec);     
+            removeDomination(essRTList, tmpSpec);
         } //while
-    
-       // System.out.println(conSpecList);
+
+        // System.out.println(conSpecList);
         return conSpecList;
     }
-    
-    
-    
+
     /**
-     * 
+     *
      * @param rtList a list of rticators
-     * @param spec a species
-     * After calling the method, the rticators in the list which is
-     * 'adjacent' to the species are removed 
+     * @param spec a species After calling the method, the rticators in the list
+     * which is 'adjacent' to the species are removed
      */
-    
-    
-    private void removeDomination(ArrayList<ResourceType> rtList, Species spec){
+    private void removeDomination(ArrayList<ResourceType> rtList, Species spec) {
         rtList.removeAll(spec.rtList);
     }
-    
+
     //for test
     /*
     
@@ -381,8 +326,6 @@ public class SpecRTGraph {
       //  System.out.println("The number of redundance candidates are"+redCandidate.size());
     }
     * */
-    
-    
     //for test
     /*
     public void rticatorCoverTest(){
@@ -404,21 +347,19 @@ public class SpecRTGraph {
         System.out.println("The number of redundacne rticators is "+redRT.size());
     }
     * */
-    
-    
-    public String toString(){
-         StringBuilder outStr=new StringBuilder("Species-ResourceType graph: \n ");
-                  
-         for(Species spec : specNode){
+    public String toString() {
+        StringBuilder outStr = new StringBuilder("Species-ResourceType graph: \n ");
+
+        for (Species spec : specNode) {
             //outStr.append(spec);
-             for(ResourceType rt: rtNode){
-                 if(rt.adjacentTo(spec)){
-                     outStr.append("(").append(spec.getName());
-                     outStr.append(",").append(rt.getID()).append(") ");
-                 }
-             }
+            for (ResourceType rt : rtNode) {
+                if (rt.adjacentTo(spec)) {
+                    outStr.append("(").append(spec.getName());
+                    outStr.append(",").append(rt.getID()).append(") ");
+                }
+            }
         }
-         
-         return outStr.toString();
+
+        return outStr.toString();
     }
 }
--- src/speciesselection/SpeciesSelection.java
+++ src/speciesselection/SpeciesSelection.java
@@ -17,166 +17,122 @@ import java.util.*;
 public class SpeciesSelection {
 
     /**
-     * @param args the command line arguments
-     * The first argument is the name for the input file
-     * The second argument is the name for the output file
-     * The third argument is for the number of species sets for each size
-     * The fourth argument for the minimal size of species in a species set
-     * The fifth argument for the maximal size of species in a species set
+     * @param args the command line arguments The first argument is the name for
+     * the input file The second argument is the name for the output file The
+     * third argument is for the number of species sets for each size The fourth
+     * argument for the minimal size of species in a species set The fifth
+     * argument for the maximal size of species in a species set
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
         System.out.println("The Indicator Species Selection Project.");
         System.out.println("By Taoyang Wu, Version 0.2, March 2014");
-        
-        
-        //construct the bipartite graph between species and indicators.
-        SpecRTGraph sig=new SpecRTGraph();
-        
-        //the list to record the indicators that has been processed.
-        ArrayList<Integer> indicatorCreated=new ArrayList<Integer>();
-        
-        
-        String fileName="Forest_D_ALL.txt"; //default input data
-        int numWordsInName=1; //the number of words contains in the speices name
-        
-        if(args.length>0){
-            fileName=args[0];
-                       
-            numWordsInName=1;
-        }
-        
-       String outFileName=fileName+"_result.txt";  //default file for output
-        
-        if(args.length>1){
-             outFileName=args[1];            
-        }
-        
-       System.out.println("The dataset is taken from file "+fileName); 
-       
-        
 
-        
-        File f =new File(fileName);
-        Scanner input=new Scanner(f);
-        String titleInformation=input.nextLine();
-        while(input.hasNext()){
-            String text=input.nextLine();
-           // System.out.println(text);
-            processLine(text,sig,numWordsInName);
+        //construct the bipartite graph between species and indicators.
+        SpecRTGraph sig = new SpecRTGraph();
+
+        //the list to record the indicators that has been processed.
+        ArrayList<Integer> indicatorCreated = new ArrayList<>();
+
+        String fileName = "Forest_D_ALL.txt"; //default input data
+        int numWordsInName = 1; //the number of words contains in the speices name
+
+        if (args.length > 0) {
+            fileName = args[0];
+
+            numWordsInName = 1;
         }
-      // System.out.println(sig);
-        //sig.testBelow();
-        
-       //sig.indicatorCoverTest();
-      // System.out.println("isolation test");
-      // sig.noIsolatedIndicator();
-      //sig.getEssentialIndicators();   
-      // System.out.println("consititute species.");
-       //sig.getOneMinimalConstituteSpecies();
-       //sig.getMinSpecTree();
-        // System.out.println("minDom starts");
-        // System.out.println(sig.getMinDomSpecSets());
-        // System.out.println("Complete minDom");
-       //sig.getDomSpecSets(20, 10);
-      // System.out.println("Done");
-       
-       
-       //Output the results to a file
-       
-       PrintStream outPut=new PrintStream(new File(outFileName));
-       outPut.println("The dataset:"+fileName);
-       outPut.println("The dataset contains "+sig.numSpecies()+ " species and " 
-               +sig.numResourceTypes()+ " resource types");
-       
-       int startSize=2;
-       //int endSize=1;
-       int endSize=sig.numSpecies()-1;
-      
-       int numOfSpeciesSet=11; //the number of species sets for 
-                               //each size, 11 is the default size;
-       
-       if(args.length>2){
-             numOfSpeciesSet=Integer.parseInt(args[2])+1;            
+
+        String outFileName = fileName + "_result.txt";  //default file for output
+
+        if (args.length > 1) {
+            outFileName = args[1];
         }
-       
-       if(args.length>3){
-           startSize=Integer.parseInt(args[3]);
-           endSize=startSize;
-       }
-       
-       if(args.length>4){
-           endSize=Integer.parseInt(args[4]);
-       }
-       
-    
-       MinSpecSetFamily mssf=sig.getMinDomSpecSets();
-       
-       for(int i=startSize;i<=endSize;i++){    
-      // System.out.println("In the main loop "+i);
-       SpecSetFamily consFamily=sig.getDomSpecSets(i, numOfSpeciesSet,mssf);
-       outPut.println("For "+i+"  species");
-       outPut.println(consFamily);
-       }
-       
-         System.out.println("The output is stored in "+outFileName);
-       
-      // CombSubSet mySet=new CombSubSet(3,5);
-       //System.out.println(mySet);
-       
-         // CombSize mySize=new CombSize(1000,10);
-          //System.out.println("The siz eis "+mySize.getSize());
-      // sig.printSpeices();
-        
-        
-        
+
+        System.out.println("The dataset is taken from file " + fileName);
+
+        File f = new File(fileName);
+        Scanner input = new Scanner(f);
+        String titleInformation = input.nextLine();
+        while (input.hasNext()) {
+            String text = input.nextLine();
+            processLine(text, sig, numWordsInName);
+        }
+
+        //Output the results to a file
+        PrintStream outPut = new PrintStream(new File(outFileName));
+        outPut.println("The dataset:" + fileName);
+        outPut.println("The dataset contains " + sig.numSpecies() + " species and "
+                + sig.numResourceTypes() + " resource types");
+
+        int startSize = 2;
+        int endSize = sig.numSpecies() - 1;
+
+        int numOfSpeciesSet = 11; //the number of species sets for 
+        //each size, 11 is the default size;
+
+        if (args.length > 2) {
+            numOfSpeciesSet = Integer.parseInt(args[2]) + 1;
+        }
+
+        if (args.length > 3) {
+            startSize = Integer.parseInt(args[3]);
+            endSize = startSize;
+        }
+
+        if (args.length > 4) {
+            endSize = Integer.parseInt(args[4]);
+        }
+
+        MinSpecSetFamily mssf = sig.getMinDomSpecSets();
+
+        for (int i = startSize; i <= endSize; i++) {
+            // System.out.println("In the main loop "+i);
+            SpecSetFamily consFamily = sig.getDomSpecSets(i, numOfSpeciesSet, mssf);
+            outPut.println("For " + i + "  species");
+            outPut.println(consFamily);
+        }
+        System.out.println("The output is stored in " + outFileName);
     } //main 
-    
+
     //process the given String for a line of the data file
-    public static void processLine(String text, SpecRTGraph sig,int numWordsInName){
-        Scanner data=new Scanner(text);
+    public static void processLine(String text, SpecRTGraph sig, int numWordsInName) {
+        Scanner data = new Scanner(text);
         //creat a new species
-        String spName=data.next();//+" "+data.next();
-        if(numWordsInName==2){
-            spName=spName+" "+data.next();
+        String spName = data.next();//+" "+data.next();
+        if (numWordsInName == 2) {
+            spName = spName + " " + data.next();
         }
-        Species inSpec=new Species(spName);
-        
-        
-        int spReliance=data.nextInt();
-        int spResources=data.nextInt();
-        int spSensitivity=data.nextInt();
+        Species inSpec = new Species(spName);
+
+        int spReliance = data.nextInt();
+        int spResources = data.nextInt();
+        int spSensitivity = data.nextInt();
         inSpec.setReliance(spReliance);
-       
-        
+
         //the indication inforamtion
-        int index=0;
-        
-        
+        int index = 0;
+
         //test variable
-      
-        
-        while(data.hasNextInt()){
+        while (data.hasNextInt()) {
             index++;
-            int indValue=data.nextInt();
-            if(indValue==1){
+            int indValue = data.nextInt();
+            if (indValue == 1) {
                 //first to check whether an indicator with the given id exists
-              //System.out.println("The index is "+index+".");
-                ResourceType tmpInd=sig.getResourceTypeByID(index);
-                      
-                                
-                if(tmpInd!=null){                   
-                    sig.addEdge(inSpec,tmpInd);
+                //System.out.println("The index is "+index+".");
+                ResourceType tmpInd = sig.getResourceTypeByID(index);
+
+                if (tmpInd != null) {
+                    sig.addEdge(inSpec, tmpInd);
+                } else {
+                    //System.out.println("no such indicator "+index);
+                    ResourceType newInd = new ResourceType(index);
+                    sig.addEdge(inSpec, newInd);
                 }
-                
-                else{
-                     //System.out.println("no such indicator "+index);
-                    ResourceType newInd=new ResourceType(index);                 
-                    sig.addEdge(inSpec,newInd);
-                }                
             }
-        }         
-        
+        }
+
     } //process a line
-            
+
 }
